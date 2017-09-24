a66ea9b37643b866c13f0bd55865d703e9ea925b
coin-monster <coin.monster.96@outlook.com>
2017-09-24 23:46:07 +0800

    09th days created z-index helper function

---
+++ bitimulate-frontend/src/components/atoms/ScreenMask/ScreenMask.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './ScreenMask.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const ScreenMask = () => {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default ScreenMask;
