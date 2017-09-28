24d4b761917b5d26e6fc645b6e84f15f6aafbcf8
coin-monster <coin.monster.96@outlook.com>
2017-09-28 16:55:22 +0800

    12th days - fixed react-hot-loader

--- bitimulate-frontend/config/webpack.config.dev.js
+++ bitimulate-frontend/config/webpack.config.dev.js
@@ -151,6 +151,9 @@ module.exports = {
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
+              plugins: [
+                'react-hot-loader/babel'
+              ],
               cacheDirectory: true,
             },
           },
