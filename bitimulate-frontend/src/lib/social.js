489fc342e343de65e85f3ad92f81b13c26225a2c
coin-monster <coin.monster.96@outlook.com>
2017-10-07 18:08:00 +0800

    23 days - social login preparation

---
+++ bitimulate-frontend/src/lib/social.js
@@ -0,0 +1,30 @@
+import hello from 'hellojs';
+
+hello.init({
+    // facebook: 1456738254380084,
+    // google: '139700894213-90pmhsv3jrjaoln83f353fmjvspdibb9.apps.googleusercontent.com'
+    facebook: 166763637236028,
+    google: '897961256976-7njm046grtv679oa2gknpkfd3g2qfrki.apps.googleusercontent.com'
+}, {redirect_uri: '/redirect.html'});
+
+export default(function () {
+    return {
+        facebook: () => {
+            return new Promise((resolve, reject) => {
+                // hellojs uses Promise A+ instead of Promise, so should be wrapped by Promise
+                hello.login('facebook', { scope: 'email' }).then(
+                    auth => resolve(auth.authResponse.access_token),
+                    e => reject(e)
+                );
+            })
+        },
+        google: () => {
+            return new Promise((resolve, reject) => {
+                hello.login('google', { scope: 'email' }).then(
+                    auth => resolve(auth.authResponse.access_token),
+                    e => reject(e)
+                );
+            })
+        }
+    }
+})();
