cedf2f8ea49ea3465a11b582713dba5cb7f4922f
coin-monster <coin.monster.96@outlook.com>
2017-10-12 11:37:39 +0800

    30 days - chart data crawler(ii) - implemented worker

--- bitimulate-backend/src/crawler/index.js
+++ bitimulate-backend/src/crawler/index.js
@@ -7,15 +7,17 @@ const ChartData = require('db/models/ChartData');
 const socket = require('./socket');
 const { parseJSON, polyfill } = require('lib/common');
 const currencyPairMap = require('lib/poloniex/currencyPairMap');
+const currencyPairs = require('lib/poloniex/currencyPairs');
 const progress = require('cli-progress');
+const Worker = require('./workers');
 
 const initialize = async () => {
   db.connect();
-  await ChartData.drop();
+  await ChartData.drop(); // remove all chartdata
   await importData(); // daily record from 2015/01/01
   const current = (new Date()) / 1000;
-  await importData(300, current - 60 * 60 * 24 * 30); // 5min's record from last month
-  // await importData(300, current); // additional for step 1 and 2
+  await importData(300, (new Date() / 1000) - 60 * 60 * 24 * 30); // 5min's record from last month
+  await importData(300, current); // additional for step 1 and 2
   socket.connect();
 };
 
@@ -68,33 +70,42 @@ async function registerInitialExchangeRate() {
 }
 
 async function importData(period, start) {
+  
   console.log('reloading chart data...');
-
-  const bar = new progress.Bar({}, progress.Presets.shades_classic);
-  const currencyPairs = [];
-  let current = 0;
-  for (let key in currencyPairMap) {
-    currencyPairs.push(currencyPairMap[key]);
-  }
-
-  bar.start(currencyPairs.length, 0);
-  bar.update(0);
-
+  
+  // create the list of requests
   const requests = currencyPairs.map((currencyPair) => () => poloniex.getChartData(currencyPair, period, start).then(
     (data) => ChartData.massImport(currencyPair, data)
   ));
-  for (let i = 0; i < Math.ceil(currencyPairs.length / 10); i++) {
-    const promises = requests.slice(i * 10, i * 10 + 10).map(thunk => thunk());
-    
-    try {
-      await Promise.all(promises);
-      current += promises.length;
-      bar.update(current);
-    } catch (e) {
-      console.log('error!');
-    }
+  
+  // initialize progressbar
+  const bar = new progress.Bar({}, progress.Presets.shades_classic);
+  bar.start(currencyPairs.length, 0);
+  
+  let done = 0;
+
+  const notify = () => {
+    done++;
+    bar.update(done);
+    // console.log('ended.. how...');
+  };
+
+  const count = Math.ceil(requests.length / 6);
+
+  // create 6 workers
+  const workers = new Array(6).fill(0).map((_, i) => {
+    const sliced = requests.slice(count * i, count * (i + 1));
+    return new Worker(sliced, notify);
+  });
+
+  const works = workers.map(worker => worker.work());
+
+  try {
+    await Promise.all(works);
+    bar.stop();
+  } catch (e) {
+    console.log(e);
   }
-  bar.stop();
 };
 
 async function updateEntireRate() {
