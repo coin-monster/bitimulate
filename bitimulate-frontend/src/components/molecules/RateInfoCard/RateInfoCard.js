2834cd44af82c7ff49fca70c27e3ddca38e1153b
coin-monster <coin.monster.96@outlook.com>
2017-10-13 18:52:04 +0800

    41 days - designed RateInfoCard UI

---
+++ bitimulate-frontend/src/components/molecules/RateInfoCard/RateInfoCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './RateInfoCard.scss';
+import classNames from 'classnames/bind';
+import { HoverCard } from 'components';
+import PinIcon from 'react-icons/lib/ti/pin';
+
+const cx = classNames.bind(styles);
+
+const RateInfoCard = ({shortName="BTC", name="Bitcoin", value="123123", volume="123543", percentage="0.01" }) => {
+  const parsedPercentage = (Math.round(parseFloat(percentage) * 10000) / 100);
+
+  return (
+    <div className={cx('wrapper')}>
+      <HoverCard className={cx('rate-info-card')}>
+        <div className={cx('head')}>
+          <div className={cx('short-name')}>{shortName}</div>
+          <div className={cx('pin-wrapper')}><PinIcon/></div>
+        </div>
+        <div className={cx('percentage', { positive: parsedPercentage > 0, neutral: parsedPercentage === 0 })}>({parsedPercentage.toFixed(2)}%)</div>
+        <div className={cx('value')}>{value}</div>
+        <div className={cx('name')}>{name}</div>
+        <div className={cx('volume')}>
+          <b>Volume</b>
+          <span>{volume}</span>
+        </div>
+      </HoverCard>
+    </div>
+  );
+};
+
+export default RateInfoCard;
--- bitimulate-frontend/src/components/organisms/TradeIndex/TradeIndex.js
+++ bitimulate-frontend/src/components/organisms/TradeIndex/TradeIndex.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from './TradeIndex.scss';
 import classNames from 'classnames/bind';
-import { HoverCard } from 'components';
+import { RateInfoCard } from 'components';
 
 const cx = classNames.bind(styles);
 
-const TradeIndex = () => {
+const TradeIndex = ({rate}) => {
+  const rateInfoCardList = rate.map(
+    (info) => <RateInfoCard key={info.get('name')}/>
+  );
   return (
-    <div>
-      {/* <HoverCard> hungry ... </HoverCard> */}
+    <div className={cx('trade-index')}>
+      <div className={cx('inner')}>
+        {rateInfoCardList}
+      </div>
     </div>
   );
 };
