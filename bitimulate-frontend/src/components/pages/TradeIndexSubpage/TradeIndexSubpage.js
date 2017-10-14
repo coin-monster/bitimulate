4985c855da137b1489ef9a34c1f0cbdf50340ebe
coin-monster <coin.monster.96@outlook.com>
2017-10-14 14:56:22 +0800

    43 day - sort reverser

--- bitimulate-frontend/src/components/pages/TradeIndexSubpage/TradeIndexSubpage.js
+++ bitimulate-frontend/src/components/pages/TradeIndexSubpage/TradeIndexSubpage.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import styles from './TradeIndexSubpage.scss';
 import classNames from 'classnames/bind';
+import { Selector, SortReverser } from 'components';
 import { TradeIndexContainer } from 'containers';
+
 const cx = classNames.bind(styles);
+const sorterOptions = [
+  {
+    name: 'alphabet',
+    text: 'Alphabet'
+  },
+  {
+    name: 'percentage',
+    text: 'Percentage'
+  },
+  {
+    name: 'price',
+    text: 'VPrice'
+  },
+  {
+    name: 'volume',
+    text: 'Volume'
+  }
+];
 
 const TradeIndexSubpage = () => {
   return (
     <div className={cx('trade-index-subpage')}>
+      <div className={cx('options')}>
+        <div className={cx('selector')}>
+          <Selector options={sorterOptions}/>
+        </div>
+        <div className={cx('reverser')}>
+          <SortReverser asc/>
+        </div>
+      </div>
       <TradeIndexContainer/>
     </div>
   );
