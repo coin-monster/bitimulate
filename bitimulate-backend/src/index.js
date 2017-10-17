d2dac533ce81557350251a55acccb9c23676d510
coin-monster <coin.monster.96@outlook.com>
2017-10-17 17:22:30 +0800

    65 day - install koa-cors

--- bitimulate-backend/src/index.js
+++ bitimulate-backend/src/index.js
@@ -18,12 +18,16 @@ const jwtMiddleware = require('lib/middlewares/jwt');
 const ws = require('./ws');
 
 // static file
+const cors = require('koa-cors');
 const path = require('path');
 var serve = require('koa-static');
 
 db.connect();
 
 const app = websockify(new Koa());
+
+// set cross origin
+app.use(cors());
 
 app.use(compress());
 
