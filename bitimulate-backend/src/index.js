bc3d1b09535f2c63387b7aecd44e4a77e33a02f6
coin-monster <coin.monster.96@outlook.com>
2017-11-14 17:21:51 +0800

    setting credential for prod

--- bitimulate-backend/src/index.js
+++ bitimulate-backend/src/index.js
@@ -10,6 +10,8 @@ const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const compress = require('koa-compress');
 const websockify = require('koa-websocket');
+// const cors = require('koa-cors');
+
 const koaStatic = require('koa-static');
 const path = require('path');
 const fs = require('fs');
@@ -18,7 +20,7 @@ const db = require('./db');
 
 const api = require('./api');
 const jwtMiddleware = require('lib/middlewares/jwt');
-const cache = require('lib/cache');
+// const cache = require('lib/cache');
 const ws = require('./ws');
 
 const frontendBuild = path.join(__dirname, '../../bitimulate-frontend/build');
@@ -27,6 +29,26 @@ const indexPage = fs.readFileSync(indexPagePath);
 
 db.connect();
 const app = websockify(new Koa());
+
+app.use((ctx, next) => {
+  const allowedHosts = [
+    'localhost',
+    '192.169.198.141'
+  ];
+  const origin = ctx.header['origin'];
+  allowedHosts.every(el => {
+    if (!origin) return false;
+    if (origin.indexOf(el) !== -1) {
+      ctx.response.set('Access-Control-Allow-Origin', origin);
+      return false;
+    }
+    return true;
+  });
+  ctx.set('Access-Control-Allow-Credentials', true);
+  ctx.response.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept x-timebase');
+  return next();
+});
+
 app.use(compress());
 
 app.use(jwtMiddleware);
